307d9f10f6e40d642409db506b2446d17b29ad12
Helga Yuzlikeeva <yuzlikeeva.olga@gmail.com>
2016-04-15 16:11:41 +0300

    Add photo gallery

--- src/userProfile/userProfile.js
+++ src/userProfile/userProfile.js
@@ -1,3 +1,5 @@
+App = window.parent.App;
+
 (function() {
     var hash,
         data,
@@ -17,5 +19,42 @@
 
     $('.user-info').append(userTemplate(userData));
     $('.user-gallery-item').append(commentTemplate(commentsData));
-    $('.user-photo-gallery').append(photoTemplate(userData));
+    $('.slider ul').append(photoTemplate(userData));
 })();
+
+$(document).ready(function() {
+    $('.slider').each(function () { 
+        var obj = $(this);
+
+        $(obj).append('<div class="nav"></div>');
+        $(obj).find('li').each(function () {
+            $(obj).find('.nav').append('<span rel=' + $(this).index() + '></span>');
+            $(this).addClass('slider' + $(this).index());
+        });
+        $(obj).find('span').first().addClass('on');
+    });
+});
+function sliderJS (obj, sl) {
+    var ul,
+        bl,
+        step;
+
+    ul = $(sl).find('ul');
+    bl = $(sl).find('li.slider' + obj);
+    step = $(bl).width();
+    $(ul).animate({
+        marginLeft: '-' + step * (obj - 1)
+    }, 500);
+}
+$(document).on('click', '.slider .nav span', function() {
+    var sl,
+        obj;
+
+    sl = $(this).closest('.slider');
+    $(sl).find('span').removeClass('on');
+    $(this).addClass('on');
+    obj = $(this).attr('rel');
+    sliderJS(obj, sl);
+
+    return false;
+});
